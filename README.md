85c6297af2025d30854a0216b05c94baf7587151
Juliana-Oliveira-Ramos <julianaoliveiramos1993@gmail.com>
2023-09-17 15:57:01 -0300

    Aula sobre List e suas Interfaces e como usa-las

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# BDBCursoCollectionsJava
+# Bootcamp Dio Backend Java Curso Collections Java
+Esse curso possui aulas sobre collections e o que são e como usa-las e exemplos. Ministrado pela Professora Camilia Calvante.
+
---
+++ src/OperacoesBasicas/Pesquisa/Livro.java
@@ -0,0 +1,36 @@
+package OperacoesBasicas.Pesquisa;
+
+public class Livro {
+
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override // to string converte em vez de exibia a memoria da lista e exibido o conteudo
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
